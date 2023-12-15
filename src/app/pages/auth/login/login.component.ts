6d86d797cd80796ff2a6a6363156ab416eb32049
anukul <sommer.anukul99@gmail.com>
2023-12-15 23:52:07 +0100

    base design for loginForm

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'pm-login',
   standalone: true,
-  imports: [],
+    imports: [
+        FormsModule,
+        ReactiveFormsModule
+    ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(2147483647)
+      ]]
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+
+    }
+  }
 }
