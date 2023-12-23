ae4e70c56b88cd8a5384a69a814a3ade370b443e
anukul <sommer.anukul99@gmail.com>
2023-12-23 21:51:16 +0100

    added inline comments for login. added Register Button to the page.

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'pm-login',
   standalone: true,
-    imports: [
-        FormsModule,
-        ReactiveFormsModule
-    ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './login.component.html', // Template file
+  styleUrl: './login.component.scss' // Style file
 })
 export class LoginComponent {
-  loginForm: FormGroup;
+  router: Router; // Router instance
 
-  // Creating the form structure using FormBuilder
-  constructor(private formBuilder: FormBuilder) {
+  loginForm: FormGroup; // Form group for login
+
+  constructor(private formBuilder: FormBuilder, private routerService: Router) {
+    this.router = routerService; // Initialize router instance
+
+    // Creating the form structure using FormBuilder
     this.loginForm = this.formBuilder.group({
       email: ['', [
         Validators.required,
@@ -24,17 +29,21 @@ export class LoginComponent {
       password: ['', [
         Validators.required,
         Validators.minLength(8),
-        Validators.maxLength(2147483647)
+        Validators.maxLength(2147483647) // Are you sure about this maximum length?
       ]]
     });
   }
 
-  // Method to handle the submit button
+  // Method to navigate to the registration page
+  navigateToRegister() {
+    this.router.navigate(['auth/register']);
+  }
+
+  // Method to handle form submission
   submit() {
     if (this.loginForm.valid) {
-      console.log(this.loginForm.value);
+      console.log(this.loginForm.value); // Log form values if valid (to debug)
     } else {
-
     }
   }
 }
