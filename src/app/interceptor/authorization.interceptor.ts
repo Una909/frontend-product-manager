7a6726ab4039de0fea1b94b7300bd469bd5e49e4
anukul <sommer.anukul99@gmail.com>
2023-12-23 22:39:53 +0100

    inline comments for interceptor

--- src/app/interceptor/authorization.interceptor.ts
+++ src/app/interceptor/authorization.interceptor.ts
@@ -1,13 +1,16 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
+// Define an HTTP interceptor for authorization
 export const authorizationInterceptor: HttpInterceptorFn = (req, next) => {
+  // Checking if there's an access token in local storage
   if (localStorage.getItem("ACCESS_TOKEN")) {
+    // If the access token exists, attach it to the request headers
     return next(req.clone({
       setHeaders: {
         'Authorization': `Bearer ${localStorage.getItem("ACCESS_TOKEN")}`
       }
     }));
   }
+  // If there's no access token, proceed with the original request
   return next(req);
 };
-
